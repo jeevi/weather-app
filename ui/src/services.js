177bbbd314573213f766ea26f3378296d90c56f5
Chiranjeevi Balawat <cbalawat@gmail.com>
2021-05-04 19:14:37 -0700

    updated the cors anywhere url

--- ui/src/services.js
+++ ui/src/services.js
@@ -1,15 +1,18 @@
-import axios from 'axios';
+import axios from "axios";
 
-const BASE_URL = "https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location";
+const BASE_URL =
+  "https://guarded-hamlet-95273.herokuapp.com/https://www.metaweather.com/api/location";
 
-export const fetchSearchOptions = query => {
-  return axios.get(`${BASE_URL}/search/?${query}`)
-    .then(response => response)
-    .then(error => error);
-}
+export const fetchSearchOptions = (query) => {
+  return axios
+    .get(`${BASE_URL}/search/?${query}`)
+    .then((response) => response)
+    .then((error) => error);
+};
 
-export const fetchWeather = woeid => {
-  return axios.get(`${BASE_URL}/${woeid}`)
-    .then(response => response)
-    .then(error => error);
-}
+export const fetchWeather = (woeid) => {
+  return axios
+    .get(`${BASE_URL}/${woeid}`)
+    .then((response) => response)
+    .then((error) => error);
+};
